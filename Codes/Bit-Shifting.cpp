d8dbe3957f856a2e8e2bf698f4dc83b74bf86a4f
Pat Yupipat <pat_yupipat@hotmail.com>
2017-03-09 18:46:36 +0700

    Update Bit-Shifting.cpp

--- Codes/Bit-Shifting.cpp
+++ Codes/Bit-Shifting.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
-main(){
+void testbit(int d){
+     int x = (1<<3);
+     int a = (d & 1);
+     cout << "Test Shift : " << d << endl;
+     cout << "Test Bit 3 = 1 : " << ((d & x)==x) << endl;
+     (a==1)?(cout << "Odd"):(cout << "Even");
+     cout << endl;
+}
 
-return 0;
+int main(){
+    testbit(68);
+    
+    system("pause");
+    return 0;
 }
